565437b17909a203c77ad415942bb3c971cfc2f3
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-09-20 18:46:07 +0545

    chore: booking android app init

---
+++ StackNavigator.js
@@ -0,0 +1,98 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import HomeScreen from "./screens/HomeScreen";
+import SavedScreen from "./screens/SavedScreen";
+import BookingScreen from "./screens/BookingScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import { NavigationContainer } from "@react-navigation/native";
+
+const StackNavigator = () => {
+  const Tab = createBottomTabNavigator();
+  const Stack = createNativeStackNavigator();
+
+  function BottomTabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Entypo name="home" size={24} color="black" />
+              ) : (
+                <AntDesign name="home" size={24} color="black" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Saved"
+          component={SavedScreen}
+          options={{
+            tabBarLabel: "Saved",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <AntDesign name="heart" size={24} color="black" />
+              ) : (
+                <AntDesign name="hearto" size={24} color="black" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Booking"
+          component={BookingScreen}
+          options={{
+            tabBarLabel: "Booking",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="notifications" size={24} color="black" />
+              ) : (
+                <Ionicons
+                  name="notifications-outline"
+                  size={24}
+                  color="black"
+                />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="person" size={24} color="black" />
+              ) : (
+                <Ionicons name="person-outline" size={24} color="black" />
+              ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="main" component={BottomTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({});
